33c2aed0a8b395864a0f9ddd504b1c745d26bb13
SBirdNf <56570698+SBirdNf@users.noreply.github.com>
2019-12-22 17:08:48 +0800

    upload

---
+++ VocabularyBuilder-master/app/src/main/java/com.example.app.vocabularybuilder/util/HttpCallBackListener.java
@@ -0,0 +1,18 @@
+package com.example.app.vocabularybuilder.util;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+
+/**
+ * Http访问的回调接口
+ */
+public interface HttpCallBackListener {
+    /**
+     * 当Http访问完成时回调onFinish方法
+     */
+    void onFinish(InputStream inputStream);
+    /**
+     * 当Http访问失败时回调onError方法
+     */
+    void onError();
+}
